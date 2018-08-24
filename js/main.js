a4118f9f997e7239080c4a93e3154d158d08cf68
jeancharlesgraillot <graillot.jc@gmail.com>
2018-08-24 11:57:50 +0200

    html and workshop calculator javascript basis

--- js/main.js
+++ js/main.js
@@ -1,65 +1,26 @@
-// function getValue(value) {
-//   document.getElementByTagName("input").value;
-// }
+let numbers = document.getElementsByClassName("numbers");
+let operators = document.getElementsByClassName("operators");
+let display = document.getElementById("display");
+let result = "";
 
-
-function calculer(operandeGauche, operation, operandeDroite) {
-    var resultat;
-    switch (operation) {
-    case "+":
-        resultat = operandeGauche + operandeDroite;
-        break;
-    case "-":
-        resultat = operandeGauche - operandeDroite;
-        break;
-    case "*":
-        resultat = operandeGauche * operandeDroite;
-        break;
-    case "/":
-        resultat = operandeGauche / operandeDroite;
-        break;
-    }
-    return resultat;
-}
-
-
-var inputs = document.getElementsByTagName("input");
-var result = document.getElementById("affichage");
-var array = [];
-var getOperatorClass = document.getElementsByClassName("operator");
-var getEqualId = document.getElementById("equal");
-var getNumberClass = document.getElementsByClassName("number");
-
-// $('input').click(function(){
-//   var value = $(this).val();
-//   for (var i = 0; i < inputs.length; i++) {
-//     if (inputs[i].classList.contains("operator")) {
-//       break;
-//       return value;
-//     }
-//   }
-//   result.innerHTML += value;
-//   array.push(value);
-// });
-
-
-
-function getValueAndPushArray(element) {
-  result.innerHTML += element.value;
-  array.push(element.value);
-}
-
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].onclick = function() {
-    getValueAndPushArray(this);
-    // if (inputs[i].id.contains=(equal)) {
-    //
-    // }
+for (var i = 0; i < numbers.length; i++) {
+  numbers[i].onclick = function(){
+    getValue(this);
   };
 }
 
-console.log(array);
+for (var i = 0; i < operators.length; i++) {
+  operators[i].onclick = function() {
+    getValue(this);
+  };
+}
 
-if (true) {
+function getValue(element){
+  result += element.value;
+  display.innerHTML = result;
 
+};
+
+if (getValue(this) === "Clear") {
+  result = "0";
 }
