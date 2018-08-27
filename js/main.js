cd6106500332257941339a417ec3594d4624115f
jeancharlesgraillot <graillot.jc@gmail.com>
2018-08-27 20:17:29 +0200

    code commentaries added

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ let result = "";
 // let operator = "";
 
 
-
+//Browse inputs with numbers class and get their values
 for (var i = 0; i < numbers.length; i++) {
   numbers[i].onclick = function(){
      getValue(this);
@@ -17,38 +17,43 @@ for (var i = 0; i < numbers.length; i++) {
   };
 }
 
+//Browse inputs with operators class and get their values
 for (var i = 0; i < operators.length; i++) {
   operators[i].onclick = function() {
     getValue(this);
   };
 }
 
+//Function to get value of browsed inputs and display in display div
 function getValue(element){
   result += element.value;
   display.innerHTML = result;
 };
 
-
+//Function to clear what is displayed in display div
 function clearValue() {
   result = "";
   display.innerHTML = result;
 }
 
+//Eventlistener to call clearValue function
 clear.addEventListener('click', clearValue);
 
 
-
+//Function to clear previous character that is displayed in display div
 function clearPrevious() {
   result = result.substring(0,result.length-1);
   display.innerHTML = result;
 }
 
+//Eventlistener to call clearPrevious function
 back.addEventListener('click', clearPrevious);
 
-
+//Function who checks the result string, converts into numbers and do the operation
 function calcul() {
   var convertString = eval(result);
   display.innerHTML = convertString;
 }
 
+//EventListener to call calcul function when equal is clicked and display result
 equal.addEventListener('click', calcul);
